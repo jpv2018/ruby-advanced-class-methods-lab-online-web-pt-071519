6f164aba72b57d26c419cc4a5405b5c0b36db1d7
Jason Viola <jason.viola@mavs.uta.edu>
2019-09-17 15:42:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,34 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  def self.create
+    new_song = Song.new
+    new_song.save
+    new_song
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def self.new_by_name(song_name)
+    new_song = Song.new
+    new_song.name = (song_name)
+  end
+    
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
+ 
 end
